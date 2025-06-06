5cb17eb00cf0cb077ebf9d137307f30c52b98f0d
victorgarciadss <garciavictor.1308@gmail.com>
2025-06-06 20:25:49 -0300

    Feat: add post model

---
+++ src/models/Post.ts
@@ -0,0 +1,27 @@
+import { IPost } from "@/utils/interfaces/postInterfaces";
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema<IPost>({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: [String],
+        required: false
+    }
+});
+
+export const PostModel = mongoose.models.Post || mongoose.model<IPost>("Post", postSchema);
---
+++ src/utils/interfaces/postInterfaces.ts
@@ -0,0 +1,7 @@
+export interface IPost {
+    title: string;
+    content: string;
+    createdAt: Date;
+    author: string;
+    images?: string[];
+}
