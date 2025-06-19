a047d9540e31462874346213d66a4c4847fa8234
victorgarciadss <garciavictor.1308@gmail.com>
2025-06-19 12:24:48 -0300

    Feat: create initial version to table to render post to admin

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,38 @@
+"use client";
+
 import { Header } from "@/components/header";
 import styles from "./dashboard.module.css";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { IPostResponse } from "@/utils/interfaces/postInterfaces";
+import { IUser } from "@/utils/interfaces/userInterfaces";
+import { formatDate } from "@/utils/functions/formatDate";
 
 
 export default function Dashboard() {
+
+    const [posts, setPosts] = useState<IPostResponse[]>([]);
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    async function getPosts() {
+        try {
+            const response = await fetch("http://localhost:3000/api/post", {
+                method: "GET",
+                cache: "no-store"
+            });
+
+            const posts: IPostResponse[] = await response.json();
+            setPosts(posts);
+        }
+        catch(error: any) {
+            throw new Error("Erro ao buscar os posts: " + error.message);
+        }
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
     return (
         <>
             <Header />
@@ -13,7 +42,12 @@ export default function Dashboard() {
                     <nav>
                         <ul className={styles.listContainer}>
                             <li>
-                                <button className={styles.dashboardGroupButton}>Posts</button>
+                                <button
+                                    className={styles.dashboardGroupButton}
+                                    onClick={getPosts}
+                                >
+                                    Posts
+                                </button>
                             </li>
                             <li>
                                 <button className={styles.dashboardGroupButton}>Usuários</button>
@@ -26,6 +60,39 @@ export default function Dashboard() {
                         </ul>
                     </nav>
                 </aside>
+
+                <section className={styles.infoToAdminContainer}>
+                    <div className={styles.cardsToInfo}>
+                        <div className={styles.card}>
+                            <span className={styles.icon}>📝</span>
+                            <p>Total de Posts: 25</p>   
+                        </div>
+                        <div className={styles.card}>
+                            <span className={styles.icon}>👥</span>
+                            <p>Total de Usuários: 5</p>
+                            
+                        </div>
+                    </div>
+                    <table className={styles.table}>
+                        <thead>
+                            <tr className={styles.tableLine}>
+                                <th>Titulo</th>
+                                <th>Autor</th>
+                                <th>Data</th>
+                            </tr>
+                        </thead>
+                        <tbody className={styles.tableBody}>
+                            {posts.map((post: IPostResponse) => (
+                                <tr key={post._id} className={styles.tableLine}>
+                                    <td>{post.title}</td>
+                                    <td>{post.author}</td>
+                                    <td>{formatDate(post.createdAt!)}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </section>
+                
             </main>
         
         </>
